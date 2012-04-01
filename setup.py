9b80182f8a5f072790a0fc15a0583de41d9e9de3
Vasudev Kamath <kamathvasudev@gmail.com>
2012-04-01 15:59:36 +0530

    Setup.py added

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup (
+    name = "soundex",
+    version = "0.1",
+    url = "http://silpa.org.in/Soundex",
+    license = "LGPL-3.0",
+    author = "Santhosh Thottingal",
+    author_email = "santhosh.thottingal@gmail.com",
+    description = "Soundex Phonetic Code Algorithm for Indian Languages",
+    long_description ="""Soundex Phonetic Code Algorithm Demo for Indian Languages.
+ Supports all indian languages and English. Provides intra-indic string comparison""",
+    packages = find_packages('.'),
+    namespace_packages = ["soundex"],
+    include_package_data = True,
+    setup_requires = ['setuptools-git'],
+    install_requires = ['setuptools'],
+    zip_safe = False,
+    )
