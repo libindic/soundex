e9a1bd77f66eb7be460c5a4e520b8c75e537a14a
Vasudev Kamath <kamathvasudev@gmail.com>
2012-04-11 20:59:49 +0530

    Sensible module documentation

--- soundex/core.py
+++ soundex/core.py
@@ -1,25 +1,14 @@
-#! /usr/bin/env python
+'''
+  soundex.core
+  ~~~~~~~~~~~~
+
+  This module implements soundex algorithm components.
+
+  :copyright: (c) 2009-2012 by Santhosh Thottingal
+  :license: LGPL-3.0+, see LICENSE file for more details
+'''
+
 # -*- coding: utf-8 -*-
-# 
-# Copyright 2009 Santhosh Thottingal <santhosh.thottingal@gmail.com>
-# http://www.smc.org.in
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-#
-# If you find any bugs or have any suggestions email: santhosh.thottingal@gmail.com
-# URL: http://www.smc.org.in
 
 from charmap import *
 
