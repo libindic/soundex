903cf4ae389a5e968466b0a75bf4e8c4ba383fa9
Vasudev Kamath <kamathvasudev@gmail.com>
2014-05-03 10:53:37 +0530

    use pbr for packaging
    Droped old style setup.py and use pbr which is much more intelligent and
    smarter.
    
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- setup.py
+++ setup.py
@@ -2,19 +2,7 @@
 
 from setuptools import setup
 
-setup (
-    name = "soundex",
-    version = "0.3.1",
-    url = "http://silpa.org.in/Soundex",
-    license = "LGPL-3.0",
-    author = "Santhosh Thottingal",
-    author_email = "santhosh.thottingal@gmail.com",
-    description = "Soundex Phonetic Code Algorithm for Indian Languages",
-    long_description ="""Soundex Phonetic Code Algorithm Demo for Indian Languages.
- Supports all indian languages and English. Provides intra-indic string comparison""",
-    packages = ['soundex'],
-    include_package_data = True,
-    setup_requires = ['setuptools-git'],
-    install_requires = ['setuptools'],
-    zip_safe = False,
-    )
+setup(
+    setup_requires=['pbr']
+    pbr=True
+)
