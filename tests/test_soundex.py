bcbf2a36c02d6d65afce45cceca96a1893bb931e
Vasudev Kamath <kamathvasudev@gmail.com>
2014-05-20 17:55:38 +0530

    Rewrite tests
    * Tests are rewritten using unittest framework
    * Return value for unequal string is changed 2 instead of -1, which is
      return value for comparing strings with English.
    
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- tests/test_soundex.py
+++ tests/test_soundex.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import unittest
 from soundex import getInstance
 
-s = getInstance()
 
+class SoundexTest(unittest.TestCase):
+    def setUp(self):
+        self.s = getInstance()
 
-def test_soundex():
-    '''TEST: Soundex calculation'''
-    assert s.soundex('vasudev') == 'v231'
-    assert s.soundex('Rupert') == 'R163'
-    assert s.soundex(u'ಬೆಂಗಳೂರು') == u'ಬDNFQCPC'
-    assert s.soundex(u'आम्र् फल्') == u'आNPMQ000'
+    def test_soundex(self):
+        '''TEST: Soundex calculation'''
+        self.assertEqual(self.s.soundex('vasudev'), 'v231')
+        self.assertEqual(self.s.soundex('Rupert'), 'R163')
+        self.assertEqual(self.s.soundex(u'ಬೆಂಗಳೂರು'), u'ಬDNFQCPC')
+        self.assertEqual(self.s.soundex(u'आम्र् फल्'), u'आNPMQ000')
 
-
-def test_compare():
-    '''TEST: Soundex Comparison'''
-    assert s.compare('Bangalore', u'ಬೆಂಗಳೂರು') == -1
-    assert s.compare(u'ಬೆಂಗಳೂರು', u'बॆंगळूरु') == 1
-    assert s.compare(u'बॆंगळूरु', u'बॆंगळूरु') == 0
-    assert s.compare(u'बॆंगळूरु', u'आम्र् फल्') == -1
+    def test_compare(self):
+        '''TEST: Soundex Comparison'''
+        self.assertEqual(self.s.compare('Bangalore', u'ಬೆಂಗಳೂರು'), -1)
+        self.assertEqual(self.s.compare(u'ಬೆಂಗಳೂರು', u'बॆंगळूरु'), 1)
+        self.assertEqual(self.s.compare(u'बॆंगळूरु', u'बॆंगळूरु'), 0)
+        self.assertEqual(self.s.compare(u'बॆंगळूरु', u'आम्र् फल्'), 2)
