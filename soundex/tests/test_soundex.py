78d1c2d700a3ffdb3a8110421e7668a8972f5033
Jerin Philip <monu1618@gmail.com>
2016-08-08 14:39:09 +0530

    REST Capability, Tests
    soundex/__init__.py ->
        Required functions for WSME REST
    
    soundex/tests/test_soundex.py ->
        Tests for the above functions
    
    setup.cfg ->
        Entry points added at libindic.api.rest

--- soundex/tests/test_soundex.py
+++ soundex/tests/test_soundex.py
@@ -2,6 +2,7 @@
 
 import unittest
 from soundex import getInstance
+from soundex import soundex_compare, soundex_soundex
 
 
 class SoundexTest(unittest.TestCase):
@@ -20,4 +21,18 @@ class SoundexTest(unittest.TestCase):
         self.assertEqual(self.s.compare('Bangalore', u'ಬೆಂಗಳೂರು'), -1)
         self.assertEqual(self.s.compare(u'ಬೆಂಗಳೂರು', u'बॆंगळूरु'), 2)
         self.assertEqual(self.s.compare(u'बॆंगळूरु', u'बॆंगळूरु'), 0)
+        self.assertEqual(self.s.compare(u'बॆंगळूरु', u'आम्र् फल्'), -1)
+
+    def test_soundex_soundex(self):
+        '''TEST: Soundex calculation'''
+        self.assertEqual(soundex_soundex('vasudev'), 'v231')
+        self.assertEqual(soundex_soundex('Rupert'), 'R163')
+        self.assertEqual(soundex_soundex(u'ಬೆಂಗಳೂರು'), u'ಬDNFQCPC')
+        self.assertEqual(soundex_soundex(u'आम्र् फल्'), u'आNPMQ000')
+
+    def test_soundex_compare(self):
+        '''TEST: Soundex Comparison'''
+        self.assertEqual(soundex_compare('Bangalore', u'ಬೆಂಗಳೂರು'), -1)
+        self.assertEqual(soundex_compare(u'ಬೆಂಗಳೂರು', u'बॆंगळूरु'), 2)
+        self.assertEqual(soundex_compare(u'बॆंगळूरु', u'बॆंगळूरु'), 0)
         self.assertEqual(self.s.compare(u'बॆंगळूरु', u'आम्र् फल्'), -1)
