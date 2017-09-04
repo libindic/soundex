85ef312a5af868b1f9142312a463f7e9455e5a7e
Balasankar C <balasankarc@autistici.org>
2017-09-04 12:52:14 +0530

    Fix tests and add requirements

--- libindic/soundex/tests/test_soundex.py
+++ libindic/soundex/tests/test_soundex.py
@@ -1,23 +1,26 @@
+#! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-import unittest
-from soundex import getInstance
+from testtools import TestCase
+
+from .. import Soundex
 
 
-class SoundexTest(unittest.TestCase):
+class SoundexTest(TestCase):
     def setUp(self):
-        self.s = getInstance()
+        super(SoundexTest, self).setUp()
+        self.instance = Soundex()
 
     def test_soundex(self):
         '''TEST: Soundex calculation'''
-        self.assertEqual(self.s.soundex('vasudev'), 'v231')
-        self.assertEqual(self.s.soundex('Rupert'), 'R163')
-        self.assertEqual(self.s.soundex(u'ಬೆಂಗಳೂರು'), u'ಬDNFQCPC')
-        self.assertEqual(self.s.soundex(u'आम्र् फल्'), u'आNPMQ000')
+        self.assertEqual(self.instance.soundex('vasudev'), 'v231')
+        self.assertEqual(self.instance.soundex('Rupert'), 'R163')
+        self.assertEqual(self.instance.soundex(u'ಬೆಂಗಳೂರು'), u'ಬDNFQCPC')
+        self.assertEqual(self.instance.soundex(u'आम्र् फल्'), u'आNPMQ000')
 
     def test_compare(self):
         '''TEST: Soundex Comparison'''
-        self.assertEqual(self.s.compare('Bangalore', u'ಬೆಂಗಳೂರು'), -1)
-        self.assertEqual(self.s.compare(u'ಬೆಂಗಳೂರು', u'बॆंगळूरु'), 1)
-        self.assertEqual(self.s.compare(u'बॆंगळूरु', u'बॆंगळूरु'), 0)
-        self.assertEqual(self.s.compare(u'बॆंगळूरु', u'आम्र् फल्'), 2)
+        self.assertEqual(self.instance.compare('Bangalore', u'ಬೆಂಗಳೂರು'), -1)
+        self.assertEqual(self.instance.compare(u'ಬೆಂಗಳೂರು', u'बॆंगळूरु'), 1)
+        self.assertEqual(self.instance.compare(u'बॆंगळूरु', u'बॆंगळूरु'), 0)
+        self.assertEqual(self.instance.compare(u'बॆंगळूरु', u'आम्र् फल्'), 2)
